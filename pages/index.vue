8ba0f5126e19362e1ba7db263c63cdf4557a369a
sam-morgan <andersonhenderson45@gmail.com>
2024-04-30 14:32:09 -0400

    added login signup as header

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,82 @@
 <template>
-    <input placeholder="username..." v-model="username" />
-    <br>
-    <input placeholder="email..."  v-model="email" />
-    <br>
-    <input placeholder="password..." v-model="password" />
-    <br>
+    <details style="display: inline-block; user-select: none;">
+        <summary>sign-up \ log-in</summary>
 
-    <button @click="signup" :disabled="canCreateAccount">
-        create account
-    </button>
+        <input placeholder="username..." v-model="username" />
+        <br>
+        <input placeholder="email..."  v-model="email" />
+        <br>
+        <input placeholder="password..." v-model="password" />
+        <br>
+
+        <button @click="signup" :disabled="!canCreateAccount">
+            sign up
+        </button>
+        or
+        <button @click="login" :disabled="!canLogin">
+            log in
+        </button>
+    </details>
+    <hr>
 </template>
 
 <script setup lang="ts">
 import { isEmpty } from '../util/index'
-import { type UserCredentials } from '~/types'
+import { type SignupRes, type UserCredentials } from '~/types'
 
 const username = ref('')
 const email = ref('')
 const password = ref('')
+const JWTtoken = useCookie('jwt_token')
 
 const canCreateAccount = computed(() => {
-    return isEmpty(username.value) || isEmpty(email.value) || isEmpty(password.value);
+    return !(isEmpty(username.value) || isEmpty(email.value) || isEmpty(password.value));
+})
+
+const canLogin = computed(() => {
+    return !((isEmpty(username.value) && isEmpty(email.value)) || isEmpty(password.value));
 })
 
 async function signup() {
-    const res = await $fetch('/api/signup', {
-        method: 'POST',
-        body: <UserCredentials>{
-            username: username.value,
-            password: password.value,
-            email: email.value,
+    try {
+        const res: SignupRes = await $fetch('/api/signup', {
+            method: 'POST',
+            body: <UserCredentials>{
+                username: username.value,
+                password: password.value,
+                email: email.value,
+            }
+        })
+
+        // setting cookie
+        if (res.token) {
+            JWTtoken.value = res.token
         }
-    })
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+
+async function login() {
+    try {
+        const res: SignupRes = await $fetch('/api/login', {
+            method: 'POST',
+            body: <UserCredentials>{
+                username: username.value,
+                password: password.value,
+                email: email.value,
+            }
+        })
+
+        // setting cookie
+        if (res.token) {
+            JWTtoken.value = res.token
+        }
+    }
+    catch (e) {
+        console.error(e)
+    }
 }
 
 </script>
