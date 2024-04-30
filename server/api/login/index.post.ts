c17e09ec60d6307b200fa1b14a5de6984237d168
sam-morgan <andersonhenderson45@gmail.com>
2024-04-30 14:32:37 -0400

    placeholder singup login endpoints

--- server/api/login/index.post.ts
+++ server/api/login/index.post.ts
@@ -1,4 +1,18 @@
+import { SignupRes, UserCredentials } from "~/types"
+
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event)
-    console.log(body)
+    const body: UserCredentials = await readBody(event)
+    const config = useRuntimeConfig()
+
+    try {
+        // const res: SignupRes = await $fetch(`${config.public.backendBaseURL}/login`, {
+        //     method: "POST",
+        //     body
+        // })
+        // return res
+        return { token: '12345' }
+    }
+    catch (e: any) {
+        throw createError({ status: e.status, message: `login error: ${e.message}` })
+    }
 })
--- server/api/signup/index.post.ts
+++ server/api/signup/index.post.ts
@@ -1,5 +1,18 @@
-import { UserCredentials } from "~/types"
+import { SignupRes, UserCredentials } from "~/types"
 
 export default defineEventHandler(async (event) => {
     const body: UserCredentials = await readBody(event)
+    const config = useRuntimeConfig()
+
+    try {
+        // const res: SignupRes = await $fetch(`${config.public.backendBaseURL}/signup`, {
+        //     method: "POST",
+        //     body
+        // })
+        // return res
+        return { token: '12345' }
+    }
+    catch (e: any) {
+        throw createError({ status: e.status, message: `signup error: ${e.message}` })
+    }
 })
