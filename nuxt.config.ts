8b35485ec0e3dfdfd69c9e9dfa4c9d554aa80a8d
sam-morgan <andersonhenderson45@gmail.com>
2024-04-30 13:26:46 -0400

    added backendBaseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      backendBaseURL: process.env.NODE_ENV === "development" ? "localhost:8080" : process.env.BACKEND_BASE_URL
+    }
+  }
 })
