987ef162a515565b9ed0a097334ee9e3e85baef8
sam-morgan <andersonhenderson45@gmail.com>
2024-05-02 12:04:24 -0400

    added login card for modal

---
+++ components/LoginCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <UCard>
+        <p class="text-s font-thin">Username</p>
+        <UInput class="mb-4" padded v-model="username"/>
+        <p class="text-s font-thin">Password</p>
+        <UInput class="mb-4" padded type="password" v-model="password"/>
+        <UButton @click="$emit('login', username, password)" :disabled="!canLogin">Login</UButton>
+    </UCard>
+</template>
+
+<script setup lang="ts">
+import { isEmpty } from '~/util';
+
+const username = ref('')
+const password = ref('')
+
+const canLogin = computed(() => {
+    return !(isEmpty(username.value) || isEmpty(password.value));
+})
+</script>
