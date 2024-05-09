8dd227a4859d2b3ba03ba9cfb5388693b388bd24
sam-morgan <andersonhenderson45@gmail.com>
2024-05-09 16:36:11 -0400

    added interfaces for endpoints

--- types/index.ts
+++ types/index.ts
@@ -4,10 +4,6 @@ export interface UserCredentials {
     email: string,
 };
 
-export interface SignupRes {
-    token: string,
-}
-
 export interface BookSearchInfo {
     key: string,
     author_key: string,
@@ -16,4 +12,69 @@ export interface BookSearchInfo {
     title: string,
     cover_edition_key: string,
     cover_i: string
+}
+
+export interface GetReviewRes {
+    content: string,
+    rating: number,
+    username: string,
+    worksid: string,
+    reviewid: string,
+    post_date: string,
+}
+
+
+/* /login */
+export interface LoginReq {
+    username: string,
+    password: string,
+    email: string,
+}
+
+export interface LoginRes {
+    token: string,
+}
+
+
+/* /signup */
+export interface SignupReq {
+    username: string,
+    password: string,
+    email: string,
+}
+
+export interface SignupRes {
+    token: string,
+}
+
+
+/* /user/[username] */
+export interface GetUserRes {
+    username: string,
+    email: string,
+    description: string,
+    join_date: Date
+}
+
+
+/* /review */
+export interface PostReviewReq {
+    content: string,
+    rating: number,
+    worksid: string,
+}
+
+export interface PostReviewRes {
+    reviewid: string
+}
+
+/* /review/[reviewid] */
+export interface GetReviewRes {
+    content: string,
+    username: string,
+    rating: number
+}
+
+export interface DeleteReviewReq {
+    token: string,
 }
