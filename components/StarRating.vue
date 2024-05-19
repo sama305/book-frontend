cb41d7b96bb91865ee4dd1dad7c470dacc690d44
sam-morgan <andersonhenderson45@gmail.com>
2024-05-19 13:10:38 -0400

    can now reset rating to 0

--- components/StarRating.vue
+++ components/StarRating.vue
@@ -1,15 +1,22 @@
 <template>
-    <div @mouseenter="hovering = true" @mouseleave="hovering = false" class="flex">
-        <template v-for="i in 5">
-            <UIcon
-                :size="starSize"
-                @mouseover="handleMouseOver($event, i)"
-                @click="selectStar($event, i)"
-                dynamic
-                :name="i * 2 <= hoveredStar ? `mdi:star` : (i * 2 - 1 === hoveredStar ? `mdi:star-half-full` : `mdi:star-outline`)"
-                class="star"
-            />
-        </template>
+    <div class="flex items-center">
+        <div @mouseenter="hovering = true" @mouseleave="hovering = false" class="flex">
+            <template v-for="i in 5">
+                <UIcon
+                    :size="starSize"
+                    @mouseover="handleMouseOver($event, i)"
+                    @click="selectStar($event, i)"
+                    dynamic
+                    :name="i * 2 <= hoveredStar ? `mdi:star` : (i * 2 - 1 === hoveredStar ? `mdi:star-half-full` : `mdi:star-outline`)"
+                    class="star"
+                />
+            </template>
+        </div>
+        <div class="w-4 h-8">
+            <template v-if="defaultStar != 0">
+                <UButton @click="reset" variant="link" color="black" icon="i-heroicons-x-mark-16-solid" />
+            </template>
+        </div>
     </div>
 </template>
 
@@ -44,6 +51,12 @@ function selectStar(event: MouseEvent, star: number) {
         defaultStar.value = star * 2
     }
     emit('selectStar', defaultStar.value)
+}
+
+function reset() {
+    defaultStar.value = 0
+    hoveredStar.value = 0
+    emit('selectStar', 0)
 }
 
 </script>
